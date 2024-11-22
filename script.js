bf3ad247d306726d76bab008bd4ff0abc18c1a86
MuhammadHasnain02 <www.mrhasnain9009@gmail.com>
2024-11-21 21:33:42 -0800

    alerts

---
+++ script.js
@@ -0,0 +1,53 @@
+let text_1 = "Error! Please enter a valid password"
+alert(text_1)
+
+let text_2 = "Welcome to JS Land... \n Happy Coding!"
+alert(text_2)
+
+let text_3 = "Welcome to JS Land..."
+alert(text_3)
+
+let text_4 = "Happy Coding!"
+alert(text_4)
+
+let text_5 = "Hello... I can run JS through my web browser's console"
+alert(text_5)
+
+let myName = "Jhone Doe"
+let text_6 = myName
+alert(text_6)
+
+let myAge = "15 years old"
+let text_7 = myAge
+alert(text_7)
+
+let qualification = "Certified Mobile Application Development" 
+let text_8 = qualification
+alert(text_8)
+
+let text_9 = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(text_9)
+
+let email = "example@example.com"
+let text_10 = "My email adress is " + email
+alert(text_10)
+
+let bookName = "A smarter way to learn JavaScript"
+let text_11 = "I am trying to learn from the book \n"+ bookName
+alert(text_11)
+
+let age = 15
+let text_12 = "I am "+ age + " years old"
+alert(text_12)
+
+let times = 14
+let text_13 = "You have visited this site "+ times +" times"
+alert(text_13)
+
+let birthYear = 1990
+let text_14 = "My birth year is "+birthYear
+alert(text_14)
+
+let quantity = 5
+let text_15 = "John Doe ordered "+ quantity +" T-shirt(s) on ABC Clothing store"
+alert(text_15)
